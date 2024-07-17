a7ce1875ad8c3a0f334a9be6b6d1af2919d25115
fonchu <fonchu.e.venyuy@gmail.com>
2024-07-17 06:41:28 +0100

    ui updates

---
+++ app/home/page.tsx
@@ -0,0 +1,12 @@
+import HomePage from "@/components/pages/homePage";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <script type="text/javascript" src="/script.js"></script>
+        <HomePage />
+      </div>
+    </>
+  );
+}
