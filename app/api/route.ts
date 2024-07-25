6260a209f228345a5c5da7183a055d50f1befe84
fonchu <hintekk@hintekks-MacBook-Pro.local>
2024-07-25 15:44:32 +0100

    updates

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,15 +2,15 @@ import { NextRequest, NextResponse } from "next/server";
 import { LocationClient, SearchPlaceIndexForTextCommand } from "@aws-sdk/client-location";
 import { secret } from "@aws-amplify/backend";
 export async function GET(req: NextRequest, res: any) {
-  console.log(process.env.NEXT_AWS_ACCESS_KEY)
-  console.log(process.env.NEXT_AWS_SECCRET_KEY)
-  console.log(process.env.NEXT_INDEX_NAME)
+  console.log(process.env.NEXT_PUBLIC_AWS_ACCESS_KEY)
+  console.log(process.env.NEXT_PUBLIC_AWS_SECCRET_KEY)
+  console.log(process.env.NEXT_PUBLIC_INDEX_NAME)
 
   const client = new LocationClient({
     region: "us-east-1",
     credentials: {
-      accessKeyId: secret('NEXT_AWS_ACCESS_KEY').toString(), // process.env.NEXT_AWS_ACCESS_KEY!,
-      secretAccessKey: secret('NEXT_AWS_SECCRET_KEY').toString()//process.env.NEXT_AWS_SECCRET_KEY!
+      accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY!,
+      secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECCRET_KEY!
     }
   });
 
@@ -19,7 +19,7 @@ export async function GET(req: NextRequest, res: any) {
   const latitude = parseFloat(req.url.split('?')[1].split('&')[1].split('=')[1])
 
   const command = new SearchPlaceIndexForTextCommand({
-    IndexName: secret('NEXT_INDEX_NAME').toString(),//process.env.NEXT_INDEX_NAME,
+    IndexName: process.env.NEXT_PUBLIC_INDEX_NAME,
     Text: place,
     MaxResults: 5,
     BiasPosition: [longitude, latitude],
