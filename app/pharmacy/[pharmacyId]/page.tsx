929c8eb0b60c6a12267063b6c6277ea3e8e994c2
fonchu <hintekk@hintekks-MacBook-Pro.local>
2024-07-28 13:48:35 +0100

    updates

--- app/pharmacy/[pharmacyId]/page.tsx
+++ app/pharmacy/[pharmacyId]/page.tsx
@@ -34,8 +34,8 @@ export default function Pharmacy({ params }: any) {
   async function initializeMap() {
     const map = await createMap({
       container: 'map', // An HTML Element or HTML element ID to render the map in https://maplibre.org/maplibre-gl-js/docs/API/classes/Map/
-      center: [-123.1187, 49.2819], // [Longitude, Latitude]
-      zoom: 11
+      center: [loclng, loclat], // [Longitude, Latitude]
+      zoom: 16
     });
 
     map.on('load', function () {
